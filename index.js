61527e8389d54af27c2a6f1d7f97fdbefc7d907b
shijianjun <shijianjun@zhizukj.com>
2022-08-07 15:32:31 +0800

    fix: add @f-git@pull @f-git/push

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 // 读取packgaejson
 const packageConfig = require('./package.json')
-const push = require('@git/push')
-const pull = require('@git/pull')
+const push = require('@f-git/push')
+const pull = require('@f-git/pull')
 
 const { program } = require('commander')
 
