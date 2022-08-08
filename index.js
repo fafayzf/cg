01e3d7b351775267fc1273558d3e2435ded19d7f
shijianjun <shijianjun@zhizukj.com>
2022-08-08 13:47:04 +0800

    feat: add commit, checkout stash shell

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 #!/usr/bin/env node
-// 读取packgaejson
+
 const packageConfig = require('./package.json')
 const push = require('@f-git/push')
 const pull = require('@f-git/pull')
+const checkout = require('@f-git/checkout')
+const commit = require('@f-git/commit')
 
 const { program } = require('commander')
+const stash = require('@f-git/stash')
 
 // 这样输出-V或--version就能看到版本号了
 program.version(packageConfig.version)
 
 // 使用zhizu init my
 program
-  .command('push')
+  .command('ph')
   .action(() => {
     push()
   });
 
 program
-  .command('pull')
+  .command('pl')
   .action(() => {
     pull()
   })
+
+program
+  .command('co [branch]')
+  .action((branch, options) => {
+    checkout(branch)
+  })
+
+program
+  .command('cm')
+  .action(() => {
+    commit()
+  })
+
+program
+  .command('s [pop]')
+  .action((pop) => {
+    pop = pop === 'pop' || pop === 'p' ? 'pop' : undefined
+    stash(pop)
+  })
 // program.option('-ig,--initgit', 'init git');
 
-program.parse(process.argv);
+program.parse(process.argv)
